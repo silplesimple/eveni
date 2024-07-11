b9befe63aae15d61309fb5ed74454114142331d2
silplesimple <154574815+silplesimple@users.noreply.github.com>
2024-07-11 15:38:12 +0900

    C# 3.2 using static 구문
    using static을 사용하여
    system.Console에 있는 함수를
    Console 클래스를 입력하지 않아도
    사용할 수 있게 된다
    ex)Console.WriteLine()
                   WriteLine()

---
+++ EvenIC#/EvenIC#/HelloWorld.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+using static System.Math;
+
+namespace EvenIC_
+{
+    internal class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello, World!");
+        }
+    }
+}
