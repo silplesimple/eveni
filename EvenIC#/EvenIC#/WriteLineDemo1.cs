e25b01c0cd44421dfaaae3ef958801a59119a5cc
silplesimple <154574815+silplesimple@users.noreply.github.com>
2024-07-12 16:26:54 +0900

    WriteLIneDemo.cs주석처리

--- EvenIC#/EvenIC#/WriteLineDemo1.cs
+++ EvenIC#/EvenIC#/WriteLineDemo1.cs
@@ -11,17 +11,17 @@ namespace EvenIC_
 {
     internal class WriteLineDemo1
     {
-        static void Main(string[] args)
-        {
-            //WriteLine("C#\r");
-            //WriteLine( "C#" );          
-            //Write("줄바꿈없음\r");
-            //WriteLine("줄 바꿈 포함");
-            //WriteLine("안녕 나는 ' 작은 따옴표야!");
-            //WriteLine("{0}", "Hello,C#");
-            //WriteLine("{0},{0},{1}", "Hello,C#","안녕,C#");
-            //WriteLine("튜터님 재미좀 슬슬 붙는듯?");
-            //WriteLine("{1},{0}", "hello", "C#");
-        }
+        //static void Main(string[] args)
+        //{
+        //    //WriteLine("C#\r");
+        //    //WriteLine( "C#" );          
+        //    //Write("줄바꿈없음\r");
+        //    //WriteLine("줄 바꿈 포함");
+        //    //WriteLine("안녕 나는 ' 작은 따옴표야!");
+        //    //WriteLine("{0}", "Hello,C#");
+        //    //WriteLine("{0},{0},{1}", "Hello,C#","안녕,C#");
+        //    //WriteLine("튜터님 재미좀 슬슬 붙는듯?");
+        //    //WriteLine("{1},{0}", "hello", "C#");
+        //}
     }
 }
