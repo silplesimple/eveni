fac12d54e00bd6ded4ca52caf0b531ec3d0baf2d
silplesimple <154574815+silplesimple@users.noreply.github.com>
2024-07-12 16:47:15 +0900

    C# 변수와 숫자 데이터 형식

---
+++ EvenIC#/EvenIC#/Variable.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace EvenIC_
+{
+    internal class Variable
+    {
+        //static void Main(string[] args)
+        //{
+        //    //int number;
+        //    //number = 7;
+        //    //Console.WriteLine(number);
+        //    //WriteLine(1234);
+        //    //WriteLine(3.14f);
+        //    //WriteLine('A');
+        //    //WriteLine("Hello");
+        //    //int i;
+        //    //i = 1234;
+        //    //WriteLine(i);
+        //    //int number = 7;
+        //    //Console.WriteLine("{0}", number);
+        //    //int number1, number2, number3;
+        //    //number1 = 1;
+        //    //number2 = 2;
+        //    //number3 = 3;
+        //    //number1 = number2 = number3 = 10;
+        //    //Console.WriteLine("{0},{1},{2}", number1, number2, number3);
+        //    //const int Max = 100;
+        //    //Max = 1001;
+        //    //int min = -2147483648;
+        //    //int max = +2147483647;
+        //    //WriteLine("int 변수의 최솟값:{0}",min);
+        //    //WriteLine("int 변수의 최댓값:{0}",max);
+        //    //sbyte isByte = 127;
+        //    //short iInt16 = 32767;
+        //    //int iInt32 = 2147483647;
+        //    //long iInt64 = 9223372036854775807;
+        //    //WriteLine("8비트 sbyte:{0}",isByte);
+        //    //WriteLine("16비트 short:{0}",iInt16);
+        //    //WriteLine("32비트 int:{0}", iInt32);
+        //    //WriteLine("64비트 long:{0}", iInt64);
+        //    //WriteLine("[32비트]iint 최솟값:{0}", int.MinValue);
+        //    //WriteLine("[32비트]iint 최대값:{0}", int.MaxValue);
+        //    //WriteLine("[08비트] byte 최솟값:{0}",byte.MinValue);
+        //    //WriteLine("[08비트] byte 최대값:{0}",byte.MaxValue);
+        //    //double 키워드로 실수 데이터 형식 선언 및 초기화
+        //    //double PI = 3.141592;
+        //    //Console.WriteLine("{0}", PI);
+        //    //double min = double.MinValue;
+        //    //WriteLine(min);
+        //    //double max = double.MaxValue;
+        //    //WriteLine(max);
+        //    //int? x = null;
+        //    //WriteLine(x);
+        //    //double? d = null;
+        //    //WriteLine(d);
+        //}
+
+    }
+}
