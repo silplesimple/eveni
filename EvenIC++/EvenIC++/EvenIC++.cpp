f9ed8ae24961baac526078fd36ae0a1ccd208f06
silplesimple <154574815+silplesimple@users.noreply.github.com>
2024-07-12 23:46:56 +0900

    알고리즘 test3

--- EvenIC++/EvenIC++/EvenIC++.cpp
+++ EvenIC++/EvenIC++/EvenIC++.cpp
@@ -20,29 +20,54 @@ using namespace std;
 //    return 0;
 //}
 
-int func2(int arr[], int N)
-{
-    for (int i = 0; i < N; i++)
-        for (int j = i + 1; j < N; j++)
-            if (arr[i] + arr[j] == 100)return 1;
+//int func2(int arr[], int N)
+//{
+//    for (int i = 0; i < N; i++)
+//        for (int j = i + 1; j < N; j++)
+//            if (arr[i] + arr[j] == 100)return 1;
+//    return 0;
+//}
+int func3(int N)
+{   
+    for (int i = 1; i * i <= N; i++)
+    {
+        if (i * i == N)
+            return 1;
+    }
     return 0;
+    
 }
-
-void test2()
+void test3()
 {
-    cout << "***** func2 test *******\n";
-    int arr[3][4] = { {1,52,48},{50,42},{4,13,63,87} };
-    int n[3] = { 3,2,4 };
+    cout << "******func3 test *******\n";
+    int n[3]= { 9,693953651,756580036 };
     int ans[3] = { 1,0,1 };
     for (int i = 0; i < 3; i++)
     {
-        int result = func2(arr[i], n[i]);
+        int result = func3(n[i]);
         cout << "TC #" << i << '\n';
-        cout << "expected : " << ans[i] << " result :" << result;
-        if (ans[i] == result)cout << "...Correct!\n";
-        else cout << "...Wrong!\n";
+        cout << "expected:" << ans[i] << "result:" << result;
+        if (ans[i] == result)cout << "....Correct!\n";
+        else cout << "....Wrong!\n";
     }
+    cout << "****************************\n\n";
 }
+
+//void test2()
+//{
+//    cout << "***** func2 test *******\n";
+//    int arr[3][4] = { {1,52,48},{50,42},{4,13,63,87} };
+//    int n[3] = { 3,2,4 };
+//    int ans[3] = { 1,0,1 };
+//    for (int i = 0; i < 3; i++)
+//    {
+//        int result = func2(arr[i], n[i]);
+//        cout << "TC #" << i << '\n';
+//        cout << "expected : " << ans[i] << " result :" << result;
+//        if (ans[i] == result)cout << "...Correct!\n";
+//        else cout << "...Wrong!\n";
+//    }
+//}
 //void test1()
 //{
 //    cout << "*****fun1 test *******\n";
@@ -64,5 +89,6 @@ void test2()
 int main()
 {
     //test1();
-    test2();
+    //test2();
+    test3();
 }
