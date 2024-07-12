f89bbc9de59d4166013c488c8cbd6b5102ba34ef
silplesimple <154574815+silplesimple@users.noreply.github.com>
2024-07-12 23:35:52 +0900

    알고리즘 test2

--- EvenIC++/enc_temp_folder/6519f1f76f597932da943b0ef91fcb/EvenIC++.cpp
+++ EvenIC++/enc_temp_folder/6519f1f76f597932da943b0ef91fcb/EvenIC++.cpp
@@ -25,7 +25,6 @@ int func2(int arr[], int N)
     for (int i = 0; i < N; i++)
         for (int j = i + 1; j < N; j++)
             if (arr[i] + arr[j] == 100)return 1;
-    return 0;
 }
 
 void test2()
