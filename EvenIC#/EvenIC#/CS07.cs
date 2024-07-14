3f9404892d9aec8057c7f0933cc074e509d744b2
silplesimple <154574815+silplesimple@users.noreply.github.com>
2024-07-15 02:29:36 +0900

    8강 사용자한테 얻은 정보를 변수에 저장하기

--- EvenIC#/EvenIC#/CS07.cs
+++ EvenIC#/EvenIC#/CS07.cs
@@ -9,68 +9,68 @@ namespace EvenIC_
 {
     internal class CS07
     {
-        static void Main(string[] args)
-        {
-            //char a = 'A';
-            //char b = 'B';
-            //WriteLine($"{a},{b}") ;
-            //WriteLine("{1},{0}", a, b);
-            //char grade = 'A';
-            //char kor = '가';
-            //Console.WriteLine(grade);
-            //Console.WriteLine(kor);
-            //string name = "박용준";
-            //Console.WriteLine("안녕하세요.{0}입니다.", name);
-            //Console.WriteLine(@"안녕하세요
-            //              반갑습니다
-            //            저는 심선규 라고 합니다.
-            //            잘부탁드립니다.");
-            //string message = "Hello";
-            //Console.WriteLine($"{message}");
-            //string msg = string.Format("{0}님,{1}", "백승수", "안녕하세요");
-            //Console.WriteLine(msg);
-            //string message = "String Interpolation";
-            //Console.WriteLine("Message:{0}", message);
-            //Console.WriteLine("Message:" + message);
-            //Console.WriteLine($"Message:{message}");
-            //string name = "C#";
-            //string version = "8.0";
-            //string result = String.Format("{0} {1}", name, version);
-            //string answer = String.Format($"{name} {version}");
-            //string version6 = $"{name} {version}";
-            //Console.WriteLine("{0} {1}", name, version);
-            //Console.WriteLine($"{name} {version}");
-            //Console.WriteLine(result);
-            //Console.WriteLine(answer);
-            //Console.WriteLine(version6);
-            //bool bln = true;
-            //WriteLine(bln);
-            //bool isFalse = false;
-            //Console.WriteLine(isFalse);
-            //string name = "박용준";
-            //name = "레드플러스";
-            //const int age = 20;
-            ////age = 45; //error
-            //Console.WriteLine($"{name}-{age}");
-            //const double PI = 3.14;
-            //const string SITE_NAME = "닷넷코리아";
-            //mutable==변할수 있는
-            //immutable==변할 수 없는
-            //변수는 mutable
-            //상수는 immutable
-            //Char c = 'A';
-            //string s = "안녕하세요.";
-            //Boolean b = true;
+        //static void Main(string[] args)
+        //{
+        //    //char a = 'A';
+        //    //char b = 'B';
+        //    //WriteLine($"{a},{b}") ;
+        //    //WriteLine("{1},{0}", a, b);
+        //    //char grade = 'A';
+        //    //char kor = '가';
+        //    //Console.WriteLine(grade);
+        //    //Console.WriteLine(kor);
+        //    //string name = "박용준";
+        //    //Console.WriteLine("안녕하세요.{0}입니다.", name);
+        //    //Console.WriteLine(@"안녕하세요
+        //    //              반갑습니다
+        //    //            저는 심선규 라고 합니다.
+        //    //            잘부탁드립니다.");
+        //    //string message = "Hello";
+        //    //Console.WriteLine($"{message}");
+        //    //string msg = string.Format("{0}님,{1}", "백승수", "안녕하세요");
+        //    //Console.WriteLine(msg);
+        //    //string message = "String Interpolation";
+        //    //Console.WriteLine("Message:{0}", message);
+        //    //Console.WriteLine("Message:" + message);
+        //    //Console.WriteLine($"Message:{message}");
+        //    //string name = "C#";
+        //    //string version = "8.0";
+        //    //string result = String.Format("{0} {1}", name, version);
+        //    //string answer = String.Format($"{name} {version}");
+        //    //string version6 = $"{name} {version}";
+        //    //Console.WriteLine("{0} {1}", name, version);
+        //    //Console.WriteLine($"{name} {version}");
+        //    //Console.WriteLine(result);
+        //    //Console.WriteLine(answer);
+        //    //Console.WriteLine(version6);
+        //    //bool bln = true;
+        //    //WriteLine(bln);
+        //    //bool isFalse = false;
+        //    //Console.WriteLine(isFalse);
+        //    //string name = "박용준";
+        //    //name = "레드플러스";
+        //    //const int age = 20;
+        //    ////age = 45; //error
+        //    //Console.WriteLine($"{name}-{age}");
+        //    //const double PI = 3.14;
+        //    //const string SITE_NAME = "닷넷코리아";
+        //    //mutable==변할수 있는
+        //    //immutable==변할 수 없는
+        //    //변수는 mutable
+        //    //상수는 immutable
+        //    //Char c = 'A';
+        //    //string s = "안녕하세요.";
+        //    //Boolean b = true;
 
-            //Console.WriteLine("{0}\n{1}\n{2}", c, s, b);
-            int number1 = 1234;
-            Int32 number2 = 1234;
-            string str1 = "안녕";
-            string str2 = "안녕";
+        //    //Console.WriteLine("{0}\n{1}\n{2}", c, s, b);
+        //    int number1 = 1234;
+        //    Int32 number2 = 1234;
+        //    string str1 = "안녕";
+        //    string str2 = "안녕";
 
-            Console.WriteLine($"{number1},{number2}");
-            Console.WriteLine($"{str1},{str2}");
-            Console.WriteLine($"{DateTime.Now}");
-        }
+        //    Console.WriteLine($"{number1},{number2}");
+        //    Console.WriteLine($"{str1},{str2}");
+        //    Console.WriteLine($"{DateTime.Now}");
+        //}
     }
 }
