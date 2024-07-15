543ffe30c518fef3133e16346be04f8b7b955176
silplesimple <154574815+silplesimple@users.noreply.github.com>
2024-07-15 20:16:18 +0900

    알고리즘 1강 기초코드 작성요령

---
+++ EvenIC++/EvenIC++/BaaaaakingDog.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+void func1()//char가 최대수인 127를 넘어서 127에서 128를 더할때 -128이 되서 Integer Overflow가 일어남
+{
+	for (char s = 0; s < 128; s++)
+	{
+		cout << "hi";
+	}
+}
+
+int func2()
+{
+	int r = 1;
+	int a = 0;
+	for (int i = 1; i <= 50; i++)
+	{
+		a = r * i;
+		r = a % 61;
+	}
+	return r;
+}
+int func3()
+{
+	int a = 1;
+	int mod = 1000000007;
+	for (int i = 0; i <= 10; i++)
+		a = (long long)10*a% mod;
+	return a;
+}
+int main()
+{
+	//func1();
+	/*cout << func2();*/
+	//cout << func3();
+	/*if (0.1 + 0.1 + 0.1 == 0.3)cout << true;
+	else cout << "no no....";*/
+	/*double a = 10000000000000000001;
+	double b = 10000000000000000000;
+	if (a == b)cout << "wow..";
+	else cout << "a != b";*/
+	double a = 0.1 + 0.1 + 0.1;
+	double b = 0.3;
+	if (a == b) cout << "same 1\n";
+	if (abs(a - b) < 1e9)cout << "same 2\n";
+}
--- EvenIC++/EvenIC++/EvenIC++.cpp
+++ EvenIC++/EvenIC++/EvenIC++.cpp
@@ -1,126 +1,126 @@
-﻿// EvenIC++.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//C++은 순차 프로그램
-#include <iostream>
-using namespace std;
+﻿//// EvenIC++.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+////C++은 순차 프로그램
+//#include <iostream>
+//using namespace std;
+////
+////int func1(int N)
+////{
+////    int ret = 0;
+////    for (int i = 1; i <= N; i++)
+////    {
+////        if (i % 3 == 0 || i % 5 == 0)ret += i;
+////    }
+////    return ret;
+////}
+////int func2(int arr[], int N)//1,52,48 n=3의 맞게 하기
+////{
+////    for (int i = 0; i < N; i++)
+////        for (int j = i + 1; j < N; j++)
+////            if (arr[i] + arr[j] == 100)return 1;
+////    return 0;
+////}
 //
-//int func1(int N)
-//{
-//    int ret = 0;
-//    for (int i = 1; i <= N; i++)
+////int func2(int arr[], int N)
+////{
+////    for (int i = 0; i < N; i++)
+////        for (int j = i + 1; j < N; j++)
+////            if (arr[i] + arr[j] == 100)return 1;
+////    return 0;
+////}
+//int func3(int N)
+//{   
+//    for (int i = 1; i * i <= N; i++)
 //    {
-//        if (i % 3 == 0 || i % 5 == 0)ret += i;
+//        if (i * i == N)
+//            return 1;
 //    }
-//    return ret;
-//}
-//int func2(int arr[], int N)//1,52,48 n=3의 맞게 하기
-//{
-//    for (int i = 0; i < N; i++)
-//        for (int j = i + 1; j < N; j++)
-//            if (arr[i] + arr[j] == 100)return 1;
 //    return 0;
+//    
 //}
-
-//int func2(int arr[], int N)
+//int func4(int N)
 //{
-//    for (int i = 0; i < N; i++)
-//        for (int j = i + 1; j < N; j++)
-//            if (arr[i] + arr[j] == 100)return 1;
-//    return 0;
+//    int answer = 0;
+//    int i = 1;
+//    //for (i = 1; i <= N; i*=2)//for문 증감식 i*=2;
+//    //{
+//    //    answer = i;
+//    //}
+//    /*while (i <= N)
+//    {
+//        answer = i;
+//        i *= 2;       
+//    }*/
+//    return answer;
 //}
-int func3(int N)
-{   
-    for (int i = 1; i * i <= N; i++)
-    {
-        if (i * i == N)
-            return 1;
-    }
-    return 0;
-    
-}
-int func4(int N)
-{
-    int answer = 0;
-    int i = 1;
-    //for (i = 1; i <= N; i*=2)//for문 증감식 i*=2;
-    //{
-    //    answer = i;
-    //}
-    /*while (i <= N)
-    {
-        answer = i;
-        i *= 2;       
-    }*/
-    return answer;
-}
-void test3()
-{
-    cout << "******func3 test *******\n";
-    int n[3]= { 9,693953651,756580036 };
-    int ans[3] = { 1,0,1 };
-    for (int i = 0; i < 3; i++)
-    {
-        int result = func3(n[i]);
-        cout << "TC #" << i << '\n';
-        cout << "expected:" << ans[i] << "result:" << result;
-        if (ans[i] == result)cout << "....Correct!\n";
-        else cout << "....Wrong!\n";
-    }
-    cout << "****************************\n\n";
-}
-
-void test4()
-{
-    cout << "******func4 test ********\n";
-    int n[3] = { 5,97615282,1024 };
-    int ans[3] = { 4,67108864,1024 };
-    for (int i = 0; i < 3; i++)
-    {
-        int result = func4(n[i]);
-        cout << "TC #" << i << '\n';
-        cout << "expected:" << ans[i] << "result" << result;
-        if (ans[i] == result)cout << ".....Correct!\n";
-        else cout << ".....Wrong!\n";        
-    }
-    cout << "****************************\n\n";
-}
-
-//void test2()
+//void test3()
 //{
-//    cout << "***** func2 test *******\n";
-//    int arr[3][4] = { {1,52,48},{50,42},{4,13,63,87} };
-//    int n[3] = { 3,2,4 };
+//    cout << "******func3 test *******\n";
+//    int n[3]= { 9,693953651,756580036 };
 //    int ans[3] = { 1,0,1 };
 //    for (int i = 0; i < 3; i++)
 //    {
-//        int result = func2(arr[i], n[i]);
+//        int result = func3(n[i]);
 //        cout << "TC #" << i << '\n';
-//        cout << "expected : " << ans[i] << " result :" << result;
-//        if (ans[i] == result)cout << "...Correct!\n";
-//        else cout << "...Wrong!\n";
+//        cout << "expected:" << ans[i] << "result:" << result;
+//        if (ans[i] == result)cout << "....Correct!\n";
+//        else cout << "....Wrong!\n";
 //    }
+//    cout << "****************************\n\n";
 //}
-//void test1()
+//
+//void test4()
 //{
-//    cout << "*****fun1 test *******\n";
-//    int n[3] = { 16,34567,27639 };
-//    int ans[3] = { 60,278812814,178254968 };
-//    int result = 0;
+//    cout << "******func4 test ********\n";
+//    int n[3] = { 5,97615282,1024 };
+//    int ans[3] = { 4,67108864,1024 };
 //    for (int i = 0; i < 3; i++)
 //    {
-//        result = func1(n[i]);
+//        int result = func4(n[i]);
 //        cout << "TC #" << i << '\n';
-//        cout << "expected : " << ans[i] << " result : " << result;
-//        if (ans[i] == result) cout << "...Correct!\n";
-//        else cout << "...wrong\n";
+//        cout << "expected:" << ans[i] << "result" << result;
+//        if (ans[i] == result)cout << ".....Correct!\n";
+//        else cout << ".....Wrong!\n";        
 //    }
-//    cout << "******************************\n\n";
-//        
+//    cout << "****************************\n\n";
 //}
-
-int main()
-{
-    //test1();
-    //test2();
-    //test3();
-    test4();
-}
+//
+////void test2()
+////{
+////    cout << "***** func2 test *******\n";
+////    int arr[3][4] = { {1,52,48},{50,42},{4,13,63,87} };
+////    int n[3] = { 3,2,4 };
+////    int ans[3] = { 1,0,1 };
+////    for (int i = 0; i < 3; i++)
+////    {
+////        int result = func2(arr[i], n[i]);
+////        cout << "TC #" << i << '\n';
+////        cout << "expected : " << ans[i] << " result :" << result;
+////        if (ans[i] == result)cout << "...Correct!\n";
+////        else cout << "...Wrong!\n";
+////    }
+////}
+////void test1()
+////{
+////    cout << "*****fun1 test *******\n";
+////    int n[3] = { 16,34567,27639 };
+////    int ans[3] = { 60,278812814,178254968 };
+////    int result = 0;
+////    for (int i = 0; i < 3; i++)
+////    {
+////        result = func1(n[i]);
+////        cout << "TC #" << i << '\n';
+////        cout << "expected : " << ans[i] << " result : " << result;
+////        if (ans[i] == result) cout << "...Correct!\n";
+////        else cout << "...wrong\n";
+////    }
+////    cout << "******************************\n\n";
+////        
+////}
+//
+//int main()
+//{
+//    //test1();
+//    //test2();
+//    //test3();
+//    test4();
+//}
