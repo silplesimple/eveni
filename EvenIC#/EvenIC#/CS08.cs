468f983db21d11b5bc21f8a7608d5020da326f70
silplesimple <154574815+silplesimple@users.noreply.github.com>
2024-07-15 16:22:54 +0900

    C#09
    산술 연산자

--- EvenIC#/EvenIC#/CS08.cs
+++ EvenIC#/EvenIC#/CS08.cs
@@ -10,127 +10,127 @@ namespace EvenIC_
 {
     internal class CS08 
     {
-        static void Main(string[] args)
-        {
-            //Console.Write("이름을 입력하시오 => ");
-            //string name = Console.ReadLine();
-            //Console.WriteLine("안녕하세요.{0}님",name);
-            //int x = Console.Read();
-            //Console.WriteLine(x);
-            //Console.WriteLine(ToChar(x));//오.,...
-            //int number1 = 1234;
-            //long number2 = (long)number1;
-            //long number3 = number1;
-            //Console.WriteLine(number2);
-            //Console.WriteLine(number3);
-            //long l = long.MaxValue;
-            //Console.WriteLine($"l의 값:{l}");
-            //int i = (int)l;
-            //Console.WriteLine($"i의 값:{i}");
-            //int x = 255;
-            //byte y = (byte)x;
+        //static void Main(string[] args)
+        //{
+        //    //Console.Write("이름을 입력하시오 => ");
+        //    //string name = Console.ReadLine();
+        //    //Console.WriteLine("안녕하세요.{0}님",name);
+        //    //int x = Console.Read();
+        //    //Console.WriteLine(x);
+        //    //Console.WriteLine(ToChar(x));//오.,...
+        //    //int number1 = 1234;
+        //    //long number2 = (long)number1;
+        //    //long number3 = number1;
+        //    //Console.WriteLine(number2);
+        //    //Console.WriteLine(number3);
+        //    //long l = long.MaxValue;
+        //    //Console.WriteLine($"l의 값:{l}");
+        //    //int i = (int)l;
+        //    //Console.WriteLine($"i의 값:{i}");
+        //    //int x = 255;
+        //    //byte y = (byte)x;
 
-            //WriteLine($"{x}->{y}");
+        //    //WriteLine($"{x}->{y}");
 
-            //double d = 12.34;
-            //int i = 1234;
+        //    //double d = 12.34;
+        //    //int i = 1234;
 
-            //d = i;
-            //Console.WriteLine("암시적 형식 변환=" + d);
+        //    //d = i;
+        //    //Console.WriteLine("암시적 형식 변환=" + d);
 
-            //d = 12.34;
-            //i = (int)d;
-            //Console.WriteLine("명시적 형식 변환=" + i);
+        //    //d = 12.34;
+        //    //i = (int)d;
+        //    //Console.WriteLine("명시적 형식 변환=" + i);
 
-            //string s = "";
-            //s = Convert.ToString(d);
-            //Console.WriteLine("형식 변환=" + s);
+        //    //string s = "";
+        //    //s = Convert.ToString(d);
+        //    //Console.WriteLine("형식 변환=" + s);
 
-            //string strNumber = "1234";
-            //int number1 = Convert.ToInt32(strNumber);
-            //Console.WriteLine($"{number1}-{number1.GetType()}");
-            //int number2 = int.Parse(strNumber);
-            //Console.WriteLine($"{number2}-{number2.GetType()}");
-            //int number3 = Int32.Parse(strNumber);
-            //Console.WriteLine($"{number3}-{number3.GetType()}");
-            //int i = 1234;
-            //string s = "안녕하세요.";
-            //char c = 'A';
-            //double d = 3.14;
-            //object o = new object();
-            //Console.WriteLine(i.GetType());
-            //Console.WriteLine(s.GetType());
-            //Console.WriteLine(c.GetType());
-            //Console.WriteLine(d.GetType());
-            //Console.WriteLine(o.GetType());
-            //Console.Write("정수를 입력하세요 : ");
-            //string input = Console.ReadLine();
-            //int number = Convert.ToInt32(input);//정수가 아닌 입력이 들어오면 에러
-            //Console.WriteLine($"{number}-{number.GetType()}");
-            //Console.Write("실수를 입력하세요: ");
-            //string input = Console.ReadLine();
-            //double PI = Convert.ToDouble(input);
-            //Console.WriteLine(PI);
-            //Console.Write("문자를 입력하세요 : ");
-            //string input = Console.ReadLine();
-            //char c = Convert.ToChar(input);
-            //Console.WriteLine(c);
-            //Convert.ToString(10, 2);
-            //byte x = 10;//0000 1010
-            //Console.WriteLine($"십진수 : {x}-> 이진수:{Convert.ToString(x, 2).PadLeft(8, '0')}");
-            //Console.WriteLine($"십진수 10을 이진수로 변경: {Convert.ToString(10, 2)}");
-            //Console.WriteLine($"이진수 1010을 십진수로 변경:{Convert.ToInt32("1010", 2)}");
-            //int bin = 0b0001_0001;
-            //WriteLine(bin);
-            //int dec = 1_000_000;
-            //Console.WriteLine(dec);
+        //    //string strNumber = "1234";
+        //    //int number1 = Convert.ToInt32(strNumber);
+        //    //Console.WriteLine($"{number1}-{number1.GetType()}");
+        //    //int number2 = int.Parse(strNumber);
+        //    //Console.WriteLine($"{number2}-{number2.GetType()}");
+        //    //int number3 = Int32.Parse(strNumber);
+        //    //Console.WriteLine($"{number3}-{number3.GetType()}");
+        //    //int i = 1234;
+        //    //string s = "안녕하세요.";
+        //    //char c = 'A';
+        //    //double d = 3.14;
+        //    //object o = new object();
+        //    //Console.WriteLine(i.GetType());
+        //    //Console.WriteLine(s.GetType());
+        //    //Console.WriteLine(c.GetType());
+        //    //Console.WriteLine(d.GetType());
+        //    //Console.WriteLine(o.GetType());
+        //    //Console.Write("정수를 입력하세요 : ");
+        //    //string input = Console.ReadLine();
+        //    //int number = Convert.ToInt32(input);//정수가 아닌 입력이 들어오면 에러
+        //    //Console.WriteLine($"{number}-{number.GetType()}");
+        //    //Console.Write("실수를 입력하세요: ");
+        //    //string input = Console.ReadLine();
+        //    //double PI = Convert.ToDouble(input);
+        //    //Console.WriteLine(PI);
+        //    //Console.Write("문자를 입력하세요 : ");
+        //    //string input = Console.ReadLine();
+        //    //char c = Convert.ToChar(input);
+        //    //Console.WriteLine(c);
+        //    //Convert.ToString(10, 2);
+        //    //byte x = 10;//0000 1010
+        //    //Console.WriteLine($"십진수 : {x}-> 이진수:{Convert.ToString(x, 2).PadLeft(8, '0')}");
+        //    //Console.WriteLine($"십진수 10을 이진수로 변경: {Convert.ToString(10, 2)}");
+        //    //Console.WriteLine($"이진수 1010을 십진수로 변경:{Convert.ToInt32("1010", 2)}");
+        //    //int bin = 0b0001_0001;
+        //    //WriteLine(bin);
+        //    //int dec = 1_000_000;
+        //    //Console.WriteLine(dec);
 
-            //int hex = 0xA0_B0_C0;
-            //Console.WriteLine(hex);
-            //int number = 1234;
-            //var numbe1 = 1234;
-            //var name = "C#";
-            //Console.WriteLine(name);
+        //    //int hex = 0xA0_B0_C0;
+        //    //Console.WriteLine(hex);
+        //    //int number = 1234;
+        //    //var numbe1 = 1234;
+        //    //var name = "C#";
+        //    //Console.WriteLine(name);
 
-            //var version = 8.0;
-            //Console.WriteLine("{0:0.0}", version);
-            //var number = 1234;
-            //Console.WriteLine(number.GetType());
-            //var message = "안녕하세요";
-            //Console.WriteLine(message.GetType());
-            //var s = Console.ReadLine();
-            //var c = Convert.ToChar(Console.Read());
-            //Console.WriteLine($"{s}:{s.GetType()},{c}:{c.GetType()}");
-            //Console.WriteLine("아무키나 누르세요");
-            //ConsoleKeyInfo cki = Console.ReadKey(true);
-            //Console.WriteLine("{0}", cki.Key);
-            //Console.WriteLine("{0}", cki.KeyChar);
-            //Console.WriteLine("{0}", cki.Modifiers);
-            //if(cki.Key==ConsoleKey.Q)
-            //{
-            //    Console.WriteLine("Q를 입력하셨군요...");
-            //}
-            //int i = default;
-            //double d = default;
-            //char c = default;
-            //string s = default;
-            //Console.WriteLine($"[{i}] [{d}] [{c}] [{s}]");
-            //Console.WriteLine($"[{i}],[{d}],[{c == char.MinValue}],[{s == null}]");
-            //int intDefault = default(int);
-            //Console.WriteLine(intDefault);
-            //bool boolDefault = default(bool);
-            //Console.WriteLine(boolDefault);
-            //string strDefault = default(string);
-            //Console.WriteLine(strDefault);
-            //StringBuilder sbDefault =
-            //    default(StringBuilder);
-            //Console.WriteLine(sbDefault);
-            //var t = (100, 200);
-            //Console.WriteLine(t.Item1);
-            //Console.WriteLine(t.Item2);
-            //var (x, y) = (300, 400);
-            //Console.WriteLine($"{x},{y}");
+        //    //var version = 8.0;
+        //    //Console.WriteLine("{0:0.0}", version);
+        //    //var number = 1234;
+        //    //Console.WriteLine(number.GetType());
+        //    //var message = "안녕하세요";
+        //    //Console.WriteLine(message.GetType());
+        //    //var s = Console.ReadLine();
+        //    //var c = Convert.ToChar(Console.Read());
+        //    //Console.WriteLine($"{s}:{s.GetType()},{c}:{c.GetType()}");
+        //    //Console.WriteLine("아무키나 누르세요");
+        //    //ConsoleKeyInfo cki = Console.ReadKey(true);
+        //    //Console.WriteLine("{0}", cki.Key);
+        //    //Console.WriteLine("{0}", cki.KeyChar);
+        //    //Console.WriteLine("{0}", cki.Modifiers);
+        //    //if(cki.Key==ConsoleKey.Q)
+        //    //{
+        //    //    Console.WriteLine("Q를 입력하셨군요...");
+        //    //}
+        //    //int i = default;
+        //    //double d = default;
+        //    //char c = default;
+        //    //string s = default;
+        //    //Console.WriteLine($"[{i}] [{d}] [{c}] [{s}]");
+        //    //Console.WriteLine($"[{i}],[{d}],[{c == char.MinValue}],[{s == null}]");
+        //    //int intDefault = default(int);
+        //    //Console.WriteLine(intDefault);
+        //    //bool boolDefault = default(bool);
+        //    //Console.WriteLine(boolDefault);
+        //    //string strDefault = default(string);
+        //    //Console.WriteLine(strDefault);
+        //    //StringBuilder sbDefault =
+        //    //    default(StringBuilder);
+        //    //Console.WriteLine(sbDefault);
+        //    //var t = (100, 200);
+        //    //Console.WriteLine(t.Item1);
+        //    //Console.WriteLine(t.Item2);
+        //    //var (x, y) = (300, 400);
+        //    //Console.WriteLine($"{x},{y}");
 
-        }
+        //}
     }
 }
---
+++ EvenIC#/EvenIC#/CS09.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+namespace EvenIC_
+{
+    internal class CS09
+    {
+        static void Main(string[] args)
+        {
+            //int num = 1000;
+            //int number = num + 1234;
+            //long lNumber = (long)3.141592;
+            //Console.WriteLine(lNumber);
+            //double dNumber = (double)3.141592;
+            //WriteLine(dNumber);
+            //int i = 10;
+            //int j = 20;
+            //int k = i + j;
+
+            //Console.WriteLine(k);
+            //decimal i = 3.14M;
+            //decimal j = 3.14M;
+            //decimal k = i - j;
+            //Console.WriteLine($"{k}");
+
+            //long i = 100;
+            //long j = 200;
+            //long k = i * j;
+            //Console.WriteLine($"{k}");
+
+            //double i = 1.5;
+            //double j = 0.5;
+            //double k = i / j;
+            //Console.WriteLine($"{k}");
+            //i = 5;
+            //j = 3;
+            //double n = i % j;
+            //Console.WriteLine($"{n}");
+            //int f = 10;
+            //int s = 5;
+            //int r = f % s;
+            //Console.WriteLine("{0}%{1}={2}", f, s, r);
+            //int a = 5;
+            //int b = 3;
+            //Console.WriteLine(a + b);
+            //Console.WriteLine(a - b);
+            //Console.WriteLine(a * b);
+            //Console.WriteLine(a / b);
+            //Console.WriteLine("{0} % {1} ={2}", a, b, (a % b));
+
+            //string connect = "Hello" + "World";
+            //string connect1 = "Hi"+" "+"everyone";
+            //Console.WriteLine(connect);
+            //Console.WriteLine(connect1);
+            //Console.WriteLine("{0}+{1}, {2}", 123, 456,123+456);
+            Console.WriteLine("Hello" + "World");
+            Console.WriteLine("Hi" + " " + "everyone");
+
+            Console.WriteLine("123" + "456");
+            Console.WriteLine("123" + 456);
+            Console.WriteLine(123 + "456");
+            Console.WriteLine(123 + 456);
+            Console.WriteLine("123" + true);
+            int days = 28;            
+            Console.WriteLine("2월달은 " + days+"일입니다.");
+            Console.WriteLine("2월달은 " + days.ToString() + "일입니다.");
+            Console.WriteLine("2월달은 " + Convert.ToString(days) + "일입니다.");
+        }
+    }
+}
