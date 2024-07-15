f3c8f8ee20fc9d68caf4b566a64e1b4a10d2e2b8
silplesimple <154574815+silplesimple@users.noreply.github.com>
2024-07-16 03:03:13 +0900

    StackProject(50%)

--- Stack/Assets/Scripts/TheStack.cs
+++ Stack/Assets/Scripts/TheStack.cs
@@ -4,10 +4,28 @@ using UnityEngine;
 
 public class TheStack : MonoBehaviour
 {
+    private const float BOUNDS_SIZE = 3.5f;
+    private const float STACK_MOVING_SPEED = 5.0f;
+    private const float ERROR_MARGIN = 0.1f;
+    private const float STACK_BOUNDS_GAIN = 0.25f;
+    private const int COMBO_START_GAIN = 3;
+    
     private GameObject[] theStack;
+    private Vector2 stackBounds = new Vector2(BOUNDS_SIZE, BOUNDS_SIZE);
 
     private int stackIndex;
     private int scoreCount = 0;
+    private int combo;
+
+    private float tileTransition = 0.0f;
+    private float tileSpeed = 2.5f;
+    private float secondaryPosition;
+
+    private bool isMovingOnX = true;
+    private bool gameOver=false;
+
+    private Vector3 desiredPosition;
+    private Vector3 lastTilePosition;
     // Start is called before the first frame update
     private void Start()
     {
@@ -18,19 +36,136 @@ public class TheStack : MonoBehaviour
         stackIndex = transform.childCount - 1;
     }
 
+    private void CreateRubble(Vector3 pos,Vector3 scale)
+    {
+        GameObject go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        go.transform.localPosition = pos;
+        go.transform.localScale = scale;
+        go.AddComponent<Rigidbody>();
+    }
     // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            PlaceTile();
-            scoreCount++;
+            if(PlaceTile())
+            {
+              SpawnTile();
+              scoreCount++;
+            }
+            else
+            {
+                EndGame();
+            }
         }
+        MoveTile();
 
+        // Move the stack
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, STACK_MOVING_SPEED * Time.deltaTime);
     }
 
-    private void PlaceTile()
+    private void MoveTile()
     {
+        if(gameOver)
+        {
+            return;
+        }
+        tileTransition += Time.deltaTime * tileSpeed;
+        if(isMovingOnX)
+        theStack[stackIndex].transform.localPosition = new Vector3(Mathf.Sin(tileTransition)* BOUNDS_SIZE, scoreCount,0);
+        else
+        theStack[stackIndex].transform.localPosition = new Vector3(secondaryPosition, scoreCount, Mathf.Sin(tileTransition) * BOUNDS_SIZE);
+    }
+
+    private void SpawnTile()
+    {
+        lastTilePosition = theStack[stackIndex].transform.localPosition;
+        stackIndex--;
+        if (stackIndex<0)        
+            stackIndex = transform.childCount - 1;
+
+        desiredPosition = (Vector3.down) * scoreCount;
         theStack[stackIndex].transform.localPosition = new Vector3(0, scoreCount, 0);
+        theStack[stackIndex].transform.localScale = new Vector3(stackBounds.x,1,stackBounds.y);
+    }
+    private bool PlaceTile()
+    {
+        Transform t = theStack[stackIndex].transform;
+                
+        if(isMovingOnX)
+        {
+            float deltaX = lastTilePosition.x - t.position.x;
+            if(Mathf.Abs(deltaX)>ERROR_MARGIN)
+            {
+                //cut the Tile
+                combo = 0;
+                stackBounds.x -= Mathf.Abs(deltaX);
+                if (stackBounds.x <= 0)
+                    return false;
+
+                float middle = lastTilePosition.x + t.localPosition.x / 2;
+                t.localScale=new Vector3(stackBounds.x, 1, stackBounds.y);
+                //CreateRubble()
+                t.localPosition = new Vector3(middle - (lastTilePosition.x / 2), scoreCount, lastTilePosition.z);
+            }
+            else
+            {
+                if (combo > COMBO_START_GAIN)
+                {
+                    stackBounds.x += STACK_BOUNDS_GAIN;
+                    if (stackBounds.x > BOUNDS_SIZE)
+                        stackBounds.x = BOUNDS_SIZE;
+                    float middle = lastTilePosition.x + t.localPosition.x / 2;
+                    t.localScale = new Vector3(stackBounds.x, 1, stackBounds.y);
+                    t.localPosition = new Vector3(middle - (lastTilePosition.x / 2), scoreCount, lastTilePosition.z);
+                }
+                combo++;
+                t.localPosition = new Vector3 (lastTilePosition.x, scoreCount, lastTilePosition.z);
+            }
+        }
+        else
+        {
+            float deltaZ = lastTilePosition.z - t.position.z;
+            if (Mathf.Abs(deltaZ) > ERROR_MARGIN)
+            {
+                //cut the Tile
+                combo = 0;
+                stackBounds.y -= Mathf.Abs(deltaZ);
+                if (stackBounds.y <= 0)
+                    return false;
+
+                float middle = lastTilePosition.z + t.localPosition.z / 2;
+                t.localScale = new Vector3(stackBounds.x, 1, stackBounds.y);
+                t.localPosition = new Vector3(lastTilePosition.x, scoreCount, middle -(lastTilePosition.z/2));
+            }
+            else
+            {
+                if(combo>COMBO_START_GAIN)
+                {
+                    if (stackBounds.y > BOUNDS_SIZE)
+                        stackBounds.y = BOUNDS_SIZE;
+                    stackBounds.y += STACK_BOUNDS_GAIN;
+                    float middle = lastTilePosition.z + t.localPosition.z / 2;
+                    t.localScale = new Vector3(stackBounds.x, 1, stackBounds.y);
+                    t.localPosition = new Vector3(lastTilePosition.x, scoreCount, middle - (lastTilePosition.z / 2));
+                }
+                combo++;
+                t.localPosition = new Vector3(lastTilePosition.x, scoreCount, lastTilePosition.z);
+            }
+        }
+
+        secondaryPosition = (isMovingOnX)
+            ? t.localPosition.x
+            : t.localPosition.z;
+        isMovingOnX = !isMovingOnX;
+
+        return true;
+    }
+
+    private void EndGame()
+    {
+        Debug.Log("Lose");
+        gameOver = true;
+        theStack[stackIndex].AddComponent<Rigidbody>();
     }
 }
