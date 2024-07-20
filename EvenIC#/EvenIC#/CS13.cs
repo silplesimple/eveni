c2395b4149b83fb4ae32632aef35aecef140251a
silplesimple <154574815+silplesimple@users.noreply.github.com>
2024-07-20 22:09:34 +0900

    C#13

---
+++ EvenIC#/EvenIC#/CS13.cs
@@ -0,0 +1,137 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using static System.Console;
+
+//namespace EvenIC_
+//{
+//    internal class CS13
+//    {
+//        static void Main(string[] args)
+//        {
+//            //int kor = 100;
+//            //int eng = 90;
+
+//            //int tot = 0;
+//            //double avg = 0.0;
+
+//            //tot = kor + eng;
+//            //avg = tot / 2.0;
+
+//            //Console.WriteLine("총점 : {0}", tot);
+//            //Console.WriteLine("평균 : {0:F1}", avg);
+
+//            //int score = 60;
+
+//            //if(score>=60)
+//            //{
+//            //    Console.WriteLine("합격");
+//            //}
+
+//            //int x = 12;
+
+//            //if(x==10)
+//            //{
+//            //    Console.WriteLine($"x는 {x}입니다.");
+//            //}
+
+//            //if(x!=20)
+//            //{
+//            //    Console.WriteLine($"x는 20이 아닙니다.");
+//            //}
+//            //string s1 = "Hello.";
+//            //string s2 = "Hello.";
+//            //bool s3 = s1 == s2;
+//            //if(s1==s2)
+//            //{
+//            //    Console.WriteLine(s3);
+//            //}
+//            //bool bln = false;
+//            //if(!bln)
+//            //{
+//            //    Console.WriteLine("bln : false->!->true");
+//            //}
+//            //string name = "C#";
+//            //int version = 8;
+
+//            //if (name == "C#")
+//            //{
+//            //    if (version==8)
+//            //    {
+//            //        Console.WriteLine($"{name}{version}");
+//            //    }
+//            //}
+//            //int number = 1234;
+//            //if(number==1234&&number>=1000)
+//            //{
+//            //    Console.WriteLine("맞습니다.");
+//            //}
+            
+//            //if(number==1234||number<=1000)
+//            //{
+//            //    Console.WriteLine("하나라도 참이면 참");
+//            //}
+//            //int first = 3;
+//            //int second = 5;
+//            //int diff = 0;
+
+//            //if(first>second)
+//            //{
+//            //    diff = first - second;
+//            //}
+//            //else
+//            //{
+//            //    diff = second - first;
+//            //}
+
+//            //Console.WriteLine($"{first}와 {second}의 차이:{diff}");
+
+//            //Console.WriteLine("영문 대문자 또는 소문자 하나를 입력하세요.");
+//            //char c = Convert.ToChar(ReadLine());
+
+//            //if(c>=65&&c<=90)
+//            //{
+//            //    Console.WriteLine($"{c}는 대문자입니다.");
+//            //}
+//            //else
+//            //{
+//            //    Console.WriteLine($"{c}는 소문자입니다.");
+//            //}
+
+//            //Console.Write("문자를 입력하세요. (y/n/c) : ");
+//            //char input = Convert.ToChar(Console.ReadLine());
+//            //if(input=='y')
+//            //{
+//            //    Console.WriteLine("Yes");
+//            //}
+//            //else
+//            //{
+//            //    if(input=='n')
+//            //    {
+//            //        Console.WriteLine("NO");
+//            //    }
+//            //    else
+//            //    {
+//            //        Console.WriteLine("Cancel");
+//            //    }
+//            //}
+
+//            int number = 0;
+
+//            if(number>0)
+//            {
+//                Console.WriteLine($"{number}은 양수입니다.");
+//            }
+//            else if(number<0)
+//            {
+//                Console.WriteLine($"{number}은 음수입니다.");
+//            }
+//            else
+//            {
+//                Console.WriteLine($"{number}은 0입니다");
+//            }
+//        }
+//    }
+//}
