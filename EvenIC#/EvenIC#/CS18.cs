61e697ac6fe7be14f37fcd002b9673d4fabd4a64
silplesimple <154574815+silplesimple@users.noreply.github.com>
2024-07-25 19:51:46 +0900

    C#18(Array)

---
+++ EvenIC#/EvenIC#/CS18.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace EvenIC_
+{
+    internal class CS18
+    {
+        static void Main(string[] args)
+        {
+            //int b = 5;
+            //var array = new string[]{ "Array", "List", "Dicitonary" };
+            //foreach(var arr in array)
+            //{
+            //    Console.WriteLine(arr);
+            //}
+            //var list = new List<string> { "Array", "List", "Dictionary" };
+            //foreach(var item in list)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //var dictionary = new Dictionary<int, string>
+            //{
+            //    {0,"Array" },{1,"List"},{2,"Dictionary"}
+            //};
+            //foreach(var pair in dictionary)
+            //{                
+            //    Console.WriteLine($"{pair.Key}-{pair.Value}");
+            //    Console.WriteLine(pair.ToString());
+            //}
+            //var kor1 =90;
+            //var kor2 = 80;
+            //var kor3 = 70;
+            //var tot = kor1 + kor2 + kor3;
+            //Console.WriteLine($"총점:{tot}");
+            //string arr = "C#8";
+            //Console.WriteLine(arr[0]);
+            //Console.WriteLine(arr[1]);
+            //Console.WriteLine(arr[2]);
+            //int[] numbers;
+            //numbers = new int[3];
+            //int[] arr = new int[5];
+            //ushort[] numbers;
+            //numbers = new ushort[2];
+            //numbers[0] = 3940;
+            //numbers[1] = 2160;
+
+            //Console.WriteLine($"{numbers[0]}*{numbers[1]}");
+
+            //ushort[] numbers = new ushort[2];
+            //Console.WriteLine(numbers[1]);
+            //string[] phones;
+            //phones = new string[2];
+
+            //phones[0] = "112";
+            //phones[1] = "119";
+            //Console.WriteLine("{0},{1}", phones[0], phones[1]);
+            //int[] intArray;
+            //intArray = new int[4];
+
+            //intArray[0]=1;
+            //intArray[2] = 2;
+            //intArray[3] = 3;
+
+            ////for(int i=0;i<intArray.Length;i++)
+            ////{
+            ////    Console.WriteLine($"{i}번째 인덱스: {intArray[i]}");
+            ////}
+            //foreach(int intvalue in intArray)
+            //{
+            //    Console.WriteLine("{0}", intvalue);
+            //}
+            //int[] intArray = new int[3] { 1, 2, 3 };
+
+            //for(int i=0;i<3;i++)
+            //{
+            //    Console.WriteLine($"{i}번째 인덱스:{intArray[i]}");
+            //}
+
+            //foreach(int intvalue in intArray)
+            //{
+            //    Console.WriteLine("{0}", intvalue);
+            //}
+            //var intArray = new int[] { 1, 2, 3 };
+
+            //for(int i=0;i<3;i++)
+            //{
+            //    Console.WriteLine($"{i}번째 인덱스 : {intArray[i]}");
+            //}
+            //foreach(int intValue in intArray)
+            //{
+            //    Console.WriteLine("{0}", intValue);
+            //}
+            //int[] intArray = { 1, 2, 3 };
+
+            //for(int i=0;i<3;i++)
+            //{
+            //    Console.WriteLine($"{i}번째 인덱스 : {intArray[i]}");
+            //}
+            //foreach(int intValue in intArray)
+            //{
+            //    Console.WriteLine("{0}", intValue);
+            //}
+            //string[] languages = { "Korean", "English", "Spanish" };
+            //Console.WriteLine($"{languages[0]},{languages[1]},{languages[2]}");
+            //int[] numbers = { 1, 1_000, 10_000, 1_000_000 };
+            //foreach(int number in numbers)
+            //{
+            //    Console.WriteLine(number);
+            //}
+            //int[] numbers = { 0b1, 0B10, 0b0100, 0B_0000_1000 };
+            //foreach(var n in numbers)
+            //{
+            //    Console.WriteLine(n);
+            //}
+            char[] characters = { 'a', 'b', 'c', 'd' };
+
+        }
+    }
+}
