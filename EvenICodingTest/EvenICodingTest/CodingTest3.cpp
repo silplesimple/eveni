3cc850535885f27999af03f8170dedbfd65bc937
silplesimple <154574815+silplesimple@users.noreply.github.com>
2024-07-29 21:21:47 +0900

    Update CodingTest3.cpp

--- EvenICodingTest/EvenICodingTest/CodingTest3.cpp
+++ EvenICodingTest/EvenICodingTest/CodingTest3.cpp
@@ -3,8 +3,7 @@
 using namespace std;
 
 bool solution(string s)
-{
-    
+{    
     bool answer = true;
     int pCount = 0;
     int yCount = 0;
@@ -29,7 +28,25 @@ bool solution(string s)
     }
     return answer;
 }
+bool solution1(string s)
+{
+    int p = 0;
+    int y = 0;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == 'p' || s[i] == 'P')
+        {
+            p++;
+        }
+        else if(s[i]=='y'||s[i]=='Y')
+        {
+            y++;
+        }
+    }
+    return p==y;
+}
 int main()
 {
-    cout<< boolalpha<<solution("pPoooyY");
+    //cout<< boolalpha<<solution("pPoooyY");
+    cout << boolalpha << solution1("yY4554pP");
 }
