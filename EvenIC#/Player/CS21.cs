b3c006fa789628304a618a96c3b9c74cfbd9a085
silplesimple <154574815+silplesimple@users.noreply.github.com>
2024-07-31 16:29:20 +0900

    C#21

---
+++ EvenIC#/Player/CS21.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+using static System.Math;
+namespace Player
+{
+    internal class CS21
+    {
+        static void Main(string[] args)
+        {
+            //Math
+            double a = Math.E;
+            int b = -2;
+            b = Math.Abs(b);
+            double c=Math.Max(a, b);
+
+            //1 기본 사용 방식
+            WriteLine(Math.Pow(2, 10));
+
+            //2 using static 지시문으로 줄여서 표현
+            WriteLine(Pow(2, 10));
+            WriteLine(Max(3, 5));
+        }
+    }
+}
