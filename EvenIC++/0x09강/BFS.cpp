92b0f1215e190aa4ad9372387b0e629d556ed37b
silplesimple <154574815+silplesimple@users.noreply.github.com>
2024-08-25 21:13:30 +0900

    알고리즘.10
    재귀

---
+++ EvenIC++/0x09강/BFS.cpp
@@ -0,0 +1,15 @@
+//#include <bits/stdc++.h>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	pair<int, int>t1 = make_pair(10, 13);
+//	pair<int, int>t2 = { 4,6 };
+//	cout << t2.first << ' ' << t2.second << '\n';
+//	if (t2 < t1)cout << "t2<t1\n";
+//	else cout << "t1<t2\n";
+//	t1.swap(t2);
+//	cout << t2.first<< '\t'<<t2.second << '\n';
+//	
+//}
---
+++ EvenIC++/recursion/recursion.cpp
@@ -0,0 +1,15 @@
+//using namespace std;
+//#include <bits/stdc++.h>
+//
+//using ll = long long;
+//ll func1(ll a, ll b, ll m)
+//{
+//	ll val = 1;
+//	while (b--)val =val * a% m;
+//	return val;
+//}
+//int main()
+//{
+//	cout << func1(6, 100, 5);
+//	
+//}
