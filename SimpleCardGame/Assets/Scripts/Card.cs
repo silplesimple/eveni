fe84c02f2be75ba502273418345cc7f13c4c646c
silplesimple <154574815+silplesimple@users.noreply.github.com>
2024-08-26 17:19:51 +0900

    카드 게임 프로젝트

---
+++ SimpleCardGame/Assets/Scripts/Card.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Card
+{
+    //지금 하고 싶은 기능: 카드를 스페이드 하트 다이아 클로버로 분류하여 앞 뒷면을 가진 카드를 만들고 싶다.
+    //카드 정보를 어떻게 담으면 좋을까....
+    //정말 생각나는 게 없어서 힘들지만 용기있게 많이 익숙해 지자 넌 할 수 있어
+    //Cross Heart
+    
+
+    private GameObject[] _Hearts;
+    private GameObject[] _Spades;
+    private GameObject[] _Diamonds;
+    private GameObject[] _Clover;
+
+    //GameObject[] Harts
+    //{
+    //    //get
+    //    //{
+    //    //    _Harts = Harts;
+    //    //}
+    //    //set
+    //    //{
+    //    //    return _Harts;
+    //    //}
+    //}
+}
