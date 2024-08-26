29df92d3dfcdd3eeb7199f4abcd4e0a50010f829
silplesimple <154574815+silplesimple@users.noreply.github.com>
2024-08-26 17:16:04 +0900

    알고리즘 11
    백 트래킹

---
+++ EvenIC++/backtracking/next_permutation.cpp
@@ -0,0 +1,21 @@
+//#include<iostream>
+//#include<algorithm>
+//using namespace std;
+//int main()
+//{
+//	int myints[] = { 1,2,3 };
+//
+//	sort(myints, myints + 3);
+//
+//	cout << "The 3! possible permutations with 3 elements:\n";
+//	do {
+//	std:; cout << myints[0] << ' ' << myints[1] << ' ' << myints[2] << '\n';
+//	}
+//		while (std::next_permutation(myints, myints + 3));
+//
+//		cout << "After loop: " << myints[0] << ' ' << myints[1] << ' ' << myints[2] << '\n';
+//
+//		return 0;
+//	
+//	
+//}
