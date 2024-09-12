0eee0e77da18660437ea883f0491290721d222f4
silplesimple <154574815+silplesimple@users.noreply.github.com>
2024-09-13 01:27:49 +0900

    Update SuperHexagon

--- SuperHexagon/Assets/Rotation.cs
+++ SuperHexagon/Assets/Rotation.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Rotation : MonoBehaviour
 {
     [SerializeField]
-    float x;
+    float ParentPower = 10;
     public Vector3 vectorPos;
     // Start is called before the first frame update
     void Start()
@@ -16,12 +16,14 @@ public class Rotation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        ParentPower += 10*Time.deltaTime;
         RotationObject(PowerPos(vectorPos));
+        Debug.Log(vectorPos.x);
     }
 
     private Vector3 PowerPos(Vector3 vectorPos)
-    {
-        return vectorPos += new Vector3(10, 10, 10);
+    {        
+        return vectorPos += new Vector3(10*ParentPower, 10*ParentPower, 10*ParentPower);
     }
 
     private void RotationObject(Vector3 pos)
