99c25aefc73bc1e942e88880bb98da68794b0f4e
silplesimple <154574815+silplesimple@users.noreply.github.com>
2024-10-06 21:59:51 +0900

    알고리즘 프로젝트

---
+++ EvenICodingTest/algorithm/BFS-1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+void PrintSunGyu()
+{
+	cout << "³ª´Ù ¾Ã»õ³¢µé¾Æ!"<<'\n';
+}
+
+int main()
+{
+	for (int i = 0; i < 5; i++)
+	{
+		PrintSunGyu();
+	}
+}
