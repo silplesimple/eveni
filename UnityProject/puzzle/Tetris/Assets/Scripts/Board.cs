3d7f62b1f8f38b612c3a1df85942b8ee003f6aec
silplesimple <154574815+silplesimple@users.noreply.github.com>
2024-10-29 21:53:53 +0900

    Tetris
    spawn/Setting

---
+++ UnityProject/puzzle/Tetris/Assets/Scripts/Board.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Board : MonoBehaviour
+{
+    public Tilemap tilemap { get; private set; }
+    public TetrominoData[] tetrominoes;
+
+    private void Awake()
+    {
+        this.tilemap = GetComponentInChildren<Tilemap>();
+        for(int i=0;i<this.tetrominoes.Length;i++)
+        {
+            this.tetrominoes[i].Initalize();
+        }
+    }
+
+    private void Start()
+    {
+        SpawnPiece();
+    }
+
+    public void SpawnPiece()
+    {
+        int random = Random.Range(0, this.tetrominoes.Length);
+        TetrominoData data = this.tetrominoes[random];
+
+    }
+
+    public void Set()
+    {
+
+    }
+
+}
