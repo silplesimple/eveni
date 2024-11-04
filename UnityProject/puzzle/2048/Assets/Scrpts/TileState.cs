49bbcb47138aaac1773ee06657b47a79b7841ed6
silplesimple <154574815+silplesimple@users.noreply.github.com>
2024-11-05 00:00:27 +0900

    2048-1
    1.importingAssats
    2.CanvasSetup
    3.GridSetup
    4.Tile Prefab
    5.Tile State
    6.Tile Scripts
    7.SpawningTiles
    8.Moving Tiles
    9.Animating Tiles
    10.Merging Tiles
    11.GameOver

---
+++ UnityProject/puzzle/2048/Assets/Scrpts/TileState.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="Tile State")]
+public class TileState : ScriptableObject
+{
+    public Color backgroundColor;
+    public Color textColor;
+}
