dec34bfddb606cfed814a7875ede521340560d10
silplesimple <154574815+silplesimple@users.noreply.github.com>
2024-11-07 06:57:13 +0900

    BrickBreaker-2
    2.Paddle Movement
    3.Ball Movement
    4.Bounce Reflection
    5.Brick States
    6.Player Lives

--- UnityProject/puzzle/BrickBreaker/Assets/Scripts/GameManager.cs
+++ UnityProject/puzzle/BrickBreaker/Assets/Scripts/GameManager.cs
@@ -5,6 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public Ball ball { get; private set; }
+    public Paddle paddle { get; private set; }
+    public Brick[] Bricks { get; private set; }
+
     public int level = 1;
     public int score = 0;
     public int lives = 3;
@@ -12,6 +16,8 @@ public class GameManager : MonoBehaviour
     private void Awake()
     {
         DontDestroyOnLoad(this.gameObject);
+
+        SceneManager.sceneLoaded += OnLevelLoaded;
     }
 
     private void Start()
@@ -31,6 +37,74 @@ public class GameManager : MonoBehaviour
     {
         this.level = level;
 
+        if(level>10)
+        {
+            SceneManager.LoadScene("WinScreen");
+        }
+        else
+        {
+            SceneManager.LoadScene("Level" + level);
+        }
+
         SceneManager.LoadScene("Level"+level);
+    }
+
+    private void OnLevelLoaded(Scene scene,LoadSceneMode mode)
+    {
+        this.ball = FindObjectOfType<Ball>();
+        this.paddle = FindObjectOfType<Paddle>();
+        this.Bricks = FindObjectsOfType<Brick>();
+    }
+    private void ResetLevel()
+    {
+        this.ball.ResetBall();
+        this.paddle.ResetPaddle();
+
+        //for(int i=0;i<this.Bricks.Length;i++)
+        //{
+        //    this.Bricks[i].ResetBrick();
+        //}
+    }
+
+    private void GameOver()
+    {
+        //SceneManager.LoadScene("GameOver");
+
+        NewGame();
+    }
+    public void Miss()
+    {
+        this.lives--;
+
+        if (this.lives > 0)
+        {
+            ResetLevel();
+        }
+        else
+        {
+            GameOver();
+        }
+    }
+    public void Hit(Brick brick)
+    {
+        this.score += brick.points;
+
+        if(Cleared())
+        {
+            LoadLevel(this.level + 1);
+        }
+    }
+
+    private bool Cleared()
+    {
+        for(int i=0;i<this.Bricks.Length;i++)
+        {
+            if (this.Bricks[i] .gameObject.activeInHierarchy && !this.Bricks[i].unbreakable)
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
