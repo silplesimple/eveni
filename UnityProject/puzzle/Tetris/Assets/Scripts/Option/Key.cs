880035a9aa9ff4b2c237747f83f02103c4ca77f7
silplesimple <154574815+silplesimple@users.noreply.github.com>
2024-11-17 18:45:18 +0900

    tetris-key
    keyRebinding

---
+++ UnityProject/puzzle/Tetris/Assets/Scripts/Option/Key.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    Input input;
+
+    private void Awake()
+    {
+        input   
+
+    }
+    public void UpKey()
+    {
+        
+    }
+}
