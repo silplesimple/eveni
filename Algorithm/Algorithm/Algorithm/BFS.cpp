5d228aec7ad070388a6e3dff5e57a0aa9e13833a
silplesimple <154574815+silplesimple@users.noreply.github.com>
2024-11-26 21:18:50 +0900

    알고리즘-BFS

---
+++ Algorithm/Algorithm/Algorithm/BFS.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//페어에 대한 사용법
+//int main()
+//{
+//	pair<int, int> t1 = make_pair(10, 13);
+//	pair<int, int> t2 = { 4,6 };
+//	cout << t2.first << ' ' << t2.second << '\n';// 4 6
+//	if (t2 < t1)cout << "t2 < t1";
+//	else cout<<"t1 < t2";
+//}
